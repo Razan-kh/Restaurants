13b93082a123763997f07ea82bb0a4e22ac3d079
unknown <you@example.com>
2025-09-07 15:58:37 +0300

    req #7

---
+++ 07.sql
@@ -0,0 +1,20 @@
+USE RestaurantDB;
+GO
+
+CREATE VIEW vw_Employees AS
+SELECT 
+	e.EmployeeID,
+	e.FirstName,
+	e.LastName,
+	e.Position,
+	r.Address,
+	r.Name,
+	r.OpeningHours,
+	r.PhoneNumber,
+	r.RestaurantID
+FROM Restaurant.Restaurant AS r
+LEFT JOIN Restaurant.Employee AS e
+	ON e.RestaurantID = r.RestaurantID;
+GO
+
+SELECT * FROM vw_Employees;
